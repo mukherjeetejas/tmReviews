a16d5016991c288ba799b632bf4e4ce489a76a17
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2021-08-26 22:41:56 +0530

    server deployed on heroku @ https://tmreviews.herokuapp.com/

--- server/index.js
+++ server/index.js
@@ -16,6 +16,10 @@ app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send("welcome to reviews API")
+})
+
 app.use('/posts', postRoutes);
 
 //connect to mongoDB cloud atlas version
