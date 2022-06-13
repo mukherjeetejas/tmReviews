52c565f4583c703249ee37b6fbb2a7c54b3b5198
Tejas Mukherjee <mukherjeetejas21@gmail.com>
2022-06-13 12:16:58 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,11 +21,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/posts', postRoutes);
-
+const url = "mongodb+srv://mukherjeetejas:8Pjt3W&;Tp_75?M@cluster0.tvrvq.mongodb.net/?retryWrites=true&w=majority";
 //connect to mongoDB cloud atlas version
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log (`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
