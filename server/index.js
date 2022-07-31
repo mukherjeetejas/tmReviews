ef537c44b3d880dea4f610187d390c2103aa5615
quantumsoul <prakhar24012000@gmail.com>
2022-07-31 15:15:22 +0530

    dabase bug fix

--- server/index.js
+++ server/index.js
@@ -7,10 +7,9 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js"
 //env variables
 import dotenv from "dotenv"
-dotenv.config();
 //init the app 
 const app = express();
-
+dotenv.config();
 //setting up body parsers to properly send requests
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -21,11 +20,11 @@ app.get('/', (req, res) => {
 })
 
 app.use('/posts', postRoutes);
-const url = "mongodb+srv://mukherjeetejas:8Pjt3W&;Tp_75?M@cluster0.tvrvq.mongodb.net/?retryWrites=true&w=majority";
+const url = process.env.DATABASE;
 //connect to mongoDB cloud atlas version
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.url, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log (`Server running on port : ${PORT}`)))
     .catch((error) => console.log(error.message));
 
